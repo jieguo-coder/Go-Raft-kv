b59887a9479ad8e73c042bffaa3af3b1aadf3e96
jieguo-coder <1193249232@qq.com>
2026-05-05 23:24:24 +0800

    feat: 添加随机层级生成逻辑以支持跳表结构

--- memtable/skiplist.go
+++ memtable/skiplist.go
@@ -1,5 +1,9 @@
 package memtable
 
+import (
+	"math/rand"
+)
+
 // Node 相当于跳表里的“地铁站”（存实际的 Key 和 value）
 type Node struct {
 	key   string
@@ -11,4 +15,14 @@ type Node struct {
 type SkipList struct {
 	head     *Node
 	maxLevel int
+}
+
+// randLevel 抛硬币决定“地铁”建几层（随机算法）
+func (s *SkipList) randomLevel() int {
+	level := 1
+	// 如果抛硬币是正面 (概率0.5)，且没超过交通局规定的最高层数，就再加一层
+	for rand.Float32() < 0.5 && level < s.maxLevel {
+		level++
+	}
+	return level
 }
